ba9daa2192814a377c8b21141d267d277a59440e
noobmastersixty9 <betaylax@gmail.com>
2024-02-06 17:26:58 -0500

    Final commit

--- service/__init__.py
+++ service/__init__.py
@@ -14,8 +14,9 @@ from flask_cors import CORS
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
-CORS(app)
+
 talisman = Talisman(app)
+CORS(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
